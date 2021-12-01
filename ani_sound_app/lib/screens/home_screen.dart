178e7ba6ffdcb991744826b76224a3573c98de16
partitect <telatkaya@yandex.com>
2021-12-02 01:21:06 +0300

    first commit

---
+++ ani_sound_app/lib/screens/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    final player = AudioCache();
+    var animalList = [
+      {"name": "inek"},
+      {"name": "tavuk"},
+      {"name": "maymun"},
+      {"name": "koyun"},
+      {"name": "kartal"},
+      {"name": "yunus"},
+      {"name": "kus"},
+      {"name": "kopek"},
+      {"name": "fil"},
+      {"name": "at"},
+    ];
+    return Scaffold(
+      body: SafeArea(
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemCount: animalList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                onTap: () {
+                  player.play(
+                    '../assets/sound/${animalList[index]["name"]}.mp3',
+                  );
+                },
+                child: Container(
+                  child: Card(
+                    color: Colors.greenAccent.withOpacity(.7),
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Center(
+                            child: SvgPicture.asset(
+                              'assets/svg/${animalList[index]["name"]}.svg',
+                              width: width * .35,
+                              height: width * .35,
+                            ),
+                          ),
+                          Text(
+                            animalList[index]["name"].toString().toUpperCase(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
